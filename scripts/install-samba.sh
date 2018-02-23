4d6c64956d3536f33ef94a2fd9895ab0382607a0
ifpb <ifpb@debian>
2018-02-23 09:33:52 -0300

    atualizando o script do install samba server

--- scripts/install-samba.sh
+++
@@ -1,9 +0,0 @@
-echo "Installing Samba"
-sudo apt-get -y install samba
-cat << EOF > /etc/samba/smb.conf
-[global]
-  workgroup = GRUPO
-  security = user
-EOF
-sudo service smbd restart
-sudo service nmbd restart
--- scripts/smb-server.sh
+++ scripts/smb-server.sh
@@ -18,10 +18,10 @@ sudo service apache2 restart > /dev/null
 echo "Configurating PHP SSH2"
 # https://gist.github.com/magnetikonline/48ce1d1dca53b44666ba9332bc41c698
 sudo apt-get -y install autoconf libssh2-1-dev > /dev/null
-sudo cp /var/www/html/ssh2.so /usr/lib/php/20160303/ssh2.so
+sudo cp /var/www/html/scripts/ssh2.so /usr/lib/php/20160303/ssh2.so
 echo 'extension=ssh2.so' | sudo tee --append /etc/php/7.1/mods-available/ssh2.ini
 sudo ln -s /etc/php/7.1/mods-available/ssh2.ini /etc/php/7.1/cli/conf.d/20-ssh2.ini
-echo "\n\n[ssh2]\nextension=ssh2.so" | sudo tee --append /etc/php/7.1/apache2/php.ini
+echo -e "\n\n[ssh2]\nextension=ssh2.so" | sudo tee --append /etc/php/7.1/apache2/php.ini
 sudo service apache2 restart > /dev/null
 
 echo "Installing MySQL"
